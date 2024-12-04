f03e13579657d121f13c206b1b2a76ae4b0713af
Fernando AYON <fernando.ayon@playplay.com>
2024-12-04 08:00:29 +0100

    ✨ Solve day 4 parts 1 & 2

--- src/Day4/Day4.php
+++ src/Day4/Day4.php
@@ -17,7 +17,87 @@ final readonly class Day4 implements AdventOfCodeProblem
     {
         $solution = 0;
         $lineGenerator = DataReader::readLine(4, $this->withExampleData);
-        foreach ($lineGenerator as $line) {
+        $matrix = [];
+        $xPositions = [];
+        foreach ($lineGenerator as $ix => $line) {
+            $row = str_split($line);
+            $matrix[$ix] = $row;
+            foreach ($row as $ic => $col) {
+                if ($col === 'X') {
+                    $xPositions[$ix][$ic] = true;
+                } else {
+                    $xPositions[$ix][$ic] = false;
+                }
+            }
+        }
+
+        $rowCount = count($matrix);
+        $colCount = count($matrix[0]);
+        for ($i = 0; $i < $rowCount; $i++) {
+            for ($j = 0; $j < $colCount; $j++) {
+                // optimize a bit only going through known X positions
+                if ($xPositions[$i][$j] === false) {
+                    continue;
+                }
+
+                // we are on an X, find all M letters next to it and keep going on that direction
+                if (isset($matrix[$i - 1][$j]) && $matrix[$i - 1][$j] === 'M' // up
+                    && isset($matrix[$i - 2][$j]) && $matrix[$i - 2][$j] === 'A'
+                    && isset($matrix[$i - 3][$j]) && $matrix[$i - 3][$j] === 'S'
+                ) {
+                    $solution++;
+                }
+
+                if (isset($matrix[$i + 1][$j]) && $matrix[$i + 1][$j] === 'M' // down
+                    && isset($matrix[$i + 2][$j]) && $matrix[$i + 2][$j] === 'A'
+                    && isset($matrix[$i + 3][$j]) && $matrix[$i + 3][$j] === 'S'
+                ) {
+                    $solution++;
+                }
+
+                if (isset($matrix[$i][$j - 1]) && $matrix[$i][$j - 1] === 'M' // left
+                    && isset($matrix[$i][$j - 2]) && $matrix[$i][$j - 2] === 'A'
+                    && isset($matrix[$i][$j - 3]) && $matrix[$i][$j - 3] === 'S'
+                ) {
+                    $solution++;
+                }
+
+                if (isset($matrix[$i][$j + 1]) && $matrix[$i][$j + 1] === 'M' // right
+                    && isset($matrix[$i][$j + 2]) && $matrix[$i][$j + 2] === 'A'
+                    && isset($matrix[$i][$j + 3]) && $matrix[$i][$j + 3] === 'S'
+                ) {
+                    $solution++;
+                }
+
+                // here?
+                if (isset($matrix[$i - 1][$j - 1]) && $matrix[$i - 1][$j - 1] === 'M' // up-left
+                    && isset($matrix[$i - 2][$j - 2]) && $matrix[$i - 2][$j - 2] === 'A'
+                    && isset($matrix[$i - 3][$j - 3]) && $matrix[$i - 3][$j - 3] === 'S'
+                ) {
+                    $solution++;
+                }
+
+                if (isset($matrix[$i - 1][$j + 1]) && $matrix[$i - 1][$j + 1] === 'M' // up-right
+                    && isset($matrix[$i - 2][$j + 2]) && $matrix[$i - 2][$j + 2] === 'A'
+                    && isset($matrix[$i - 3][$j + 3]) && $matrix[$i - 3][$j + 3] === 'S'
+                ) {
+                    $solution++;
+                }
+
+                if (isset($matrix[$i + 1][$j - 1]) && $matrix[$i + 1][$j - 1] === 'M' // down-left
+                    && isset($matrix[$i + 2][$j - 2]) && $matrix[$i + 2][$j - 2] === 'A'
+                    && isset($matrix[$i + 3][$j - 3]) && $matrix[$i + 3][$j - 3] === 'S'
+                ) {
+                    $solution++;
+                }
+
+                if (isset($matrix[$i + 1][$j + 1]) && $matrix[$i + 1][$j + 1] === 'M' // down-right
+                    && isset($matrix[$i + 2][$j + 2]) && $matrix[$i + 2][$j + 2] === 'A'
+                    && isset($matrix[$i + 3][$j + 3]) && $matrix[$i + 3][$j + 3] === 'S'
+                ) {
+                    $solution++;
+                }
+            }
         }
 
         return $solution;
@@ -27,7 +107,73 @@ final readonly class Day4 implements AdventOfCodeProblem
     {
         $solution = 0;
         $lineGenerator = DataReader::readLine(4, $this->withExampleData);
-        foreach ($lineGenerator as $line) {
+        $matrix = [];
+        $aPositions = [];
+        foreach ($lineGenerator as $ix => $line) {
+            $row = str_split($line);
+            $matrix[$ix] = $row;
+            foreach ($row as $ic => $col) {
+                if ($col === 'A') {
+                    $aPositions[$ix][$ic] = true;
+                } else {
+                    $aPositions[$ix][$ic] = false;
+                }
+            }
+        }
+
+        $rowCount = count($matrix);
+        $colCount = count($matrix[0]);
+        for ($i = 0; $i < $rowCount; $i++) {
+            for ($j = 0; $j < $colCount; $j++) {
+                // optimize a bit only going through known A positions
+                if ($aPositions[$i][$j] === false) {
+                    continue;
+                }
+
+                // M . S
+                // . A .
+                // M . S
+                if (isset($matrix[$i - 1][$j - 1]) && $matrix[$i - 1][$j - 1] === 'M' // up-left
+                    && isset($matrix[$i - 1][$j + 1]) && $matrix[$i - 1][$j + 1] === 'S' // up-right
+                    && isset($matrix[$i + 1][$j - 1]) && $matrix[$i + 1][$j - 1] === 'M' // down-left
+                    && isset($matrix[$i + 1][$j + 1]) && $matrix[$i + 1][$j + 1] === 'S' // down-right
+                ) {
+                    $solution++;
+                }
+
+                // S . M
+                // . A .
+                // S . M
+                if (isset($matrix[$i - 1][$j - 1]) && $matrix[$i - 1][$j - 1] === 'S' // up-left
+                    && isset($matrix[$i - 1][$j + 1]) && $matrix[$i - 1][$j + 1] === 'M' // up-right
+                    && isset($matrix[$i + 1][$j - 1]) && $matrix[$i + 1][$j - 1] === 'S' // down-left
+                    && isset($matrix[$i + 1][$j + 1]) && $matrix[$i + 1][$j + 1] === 'M' // down-right
+                ) {
+                    $solution++;
+                }
+
+                // S . S
+                // . A .
+                // M . M
+                if (isset($matrix[$i - 1][$j - 1]) && $matrix[$i - 1][$j - 1] === 'S' // up-left
+                    && isset($matrix[$i - 1][$j + 1]) && $matrix[$i - 1][$j + 1] === 'S' // up-right
+                    && isset($matrix[$i + 1][$j - 1]) && $matrix[$i + 1][$j - 1] === 'M' // down-left
+                    && isset($matrix[$i + 1][$j + 1]) && $matrix[$i + 1][$j + 1] === 'M' // down-right
+                ) {
+                    $solution++;
+                }
+
+                // M . M
+                // . A .
+                // S . S
+                if (isset($matrix[$i - 1][$j - 1]) && $matrix[$i - 1][$j - 1] === 'M' // up-left
+                    && isset($matrix[$i - 1][$j + 1]) && $matrix[$i - 1][$j + 1] === 'M' // up-right
+                    && isset($matrix[$i + 1][$j - 1]) && $matrix[$i + 1][$j - 1] === 'S' // down-left
+                    && isset($matrix[$i + 1][$j + 1]) && $matrix[$i + 1][$j + 1] === 'S' // down-right
+                ) {
+                    $solution++;
+                }
+            }
         }
 
         return $solution;
--- tests/Day4/Day4Test.php
+++ tests/Day4/Day4Test.php
@@ -12,24 +12,24 @@ final class Day4Test extends TestCase
     public function testItSolvesTheExamplePart1(): void
     {
         // Act & Assert
-        $this->assertSame(0, new Day4(withExampleData: true)->firstPart());
+        $this->assertSame(18, new Day4(withExampleData: true)->firstPart());
     }
 
     public function testItSolvesPart1(): void
     {
         // Act & Assert
-        $this->assertSame(0, new Day4()->firstPart());
+        $this->assertSame(2458, new Day4()->firstPart());
     }
 
     public function testItSolvesTheExamplePart2(): void
     {
         // Act & Assert
-        $this->assertSame(0, new Day4(withExampleData: true)->secondPart());
+        $this->assertSame(9, new Day4(withExampleData: true)->secondPart());
     }
 
     public function testItSolvesPart2(): void
     {
         // Act & Assert
-        $this->assertSame(0, new Day4()->secondPart());
+        $this->assertSame(1945, new Day4()->secondPart());
     }
 }
