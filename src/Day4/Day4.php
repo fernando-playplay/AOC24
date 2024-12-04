385b66cde716f68eb1e07288b4d6e50e6fde9b69
Fernando AYON <fernando.ayon@playplay.com>
2024-12-04 08:53:23 +0100

    :technologist: Refactor day 4

--- src/Day4/Day4.php
+++ src/Day4/Day4.php
@@ -16,84 +16,85 @@ final readonly class Day4 implements AdventOfCodeProblem
     public function firstPart(): int
     {
         $solution = 0;
-        $lineGenerator = DataReader::readLine(4, $this->withExampleData);
-        $matrix = [];
-        $xPositions = [];
-        foreach ($lineGenerator as $ix => $line) {
-            $row = str_split($line);
-            $matrix[$ix] = $row;
-            foreach ($row as $ic => $col) {
-                if ($col === 'X') {
-                    $xPositions[$ix][$ic] = true;
-                } else {
-                    $xPositions[$ix][$ic] = false;
-                }
-            }
-        }
+        [$matrix, $xPositions] = $this->getMatrixAndMainLetterPositions(searchForLetter: 'X');
 
         $rowCount = count($matrix);
         $colCount = count($matrix[0]);
         for ($i = 0; $i < $rowCount; $i++) {
             for ($j = 0; $j < $colCount; $j++) {
-                // optimize a bit only going through known X positions
                 if ($xPositions[$i][$j] === false) {
                     continue;
                 }
 
                 // we are on an X, find all M letters next to it and keep going on that direction
-                if (isset($matrix[$i - 1][$j]) && $matrix[$i - 1][$j] === 'M' // up
-                    && isset($matrix[$i - 2][$j]) && $matrix[$i - 2][$j] === 'A'
-                    && isset($matrix[$i - 3][$j]) && $matrix[$i - 3][$j] === 'S'
+                if (
+                    isset($matrix[$i - 1][$j], $matrix[$i - 2][$j], $matrix[$i - 3][$j])
+                    && $matrix[$i - 1][$j] === 'M'
+                    && $matrix[$i - 2][$j] === 'A'
+                    && $matrix[$i - 3][$j] === 'S'
                 ) {
                     $solution++;
                 }
 
-                if (isset($matrix[$i + 1][$j]) && $matrix[$i + 1][$j] === 'M' // down
-                    && isset($matrix[$i + 2][$j]) && $matrix[$i + 2][$j] === 'A'
-                    && isset($matrix[$i + 3][$j]) && $matrix[$i + 3][$j] === 'S'
+                if (
+                    isset($matrix[$i + 1][$j], $matrix[$i + 2][$j], $matrix[$i + 3][$j])
+                    && $matrix[$i + 1][$j] === 'M' // down
+                    && $matrix[$i + 2][$j] === 'A'
+                    && $matrix[$i + 3][$j] === 'S'
                 ) {
                     $solution++;
                 }
 
-                if (isset($matrix[$i][$j - 1]) && $matrix[$i][$j - 1] === 'M' // left
-                    && isset($matrix[$i][$j - 2]) && $matrix[$i][$j - 2] === 'A'
-                    && isset($matrix[$i][$j - 3]) && $matrix[$i][$j - 3] === 'S'
+                if (
+                    isset($matrix[$i][$j - 1], $matrix[$i][$j - 2], $matrix[$i][$j - 3])
+                    && $matrix[$i][$j - 1] === 'M' // left
+                    && $matrix[$i][$j - 2] === 'A'
+                    && $matrix[$i][$j - 3] === 'S'
                 ) {
                     $solution++;
                 }
 
-                if (isset($matrix[$i][$j + 1]) && $matrix[$i][$j + 1] === 'M' // right
-                    && isset($matrix[$i][$j + 2]) && $matrix[$i][$j + 2] === 'A'
-                    && isset($matrix[$i][$j + 3]) && $matrix[$i][$j + 3] === 'S'
+                if (
+                    isset($matrix[$i][$j + 1], $matrix[$i][$j + 2], $matrix[$i][$j + 3])
+                    && $matrix[$i][$j + 1] === 'M' // right
+                    && $matrix[$i][$j + 2] === 'A'
+                    && $matrix[$i][$j + 3] === 'S'
                 ) {
                     $solution++;
                 }
 
-                // here?
-                if (isset($matrix[$i - 1][$j - 1]) && $matrix[$i - 1][$j - 1] === 'M' // up-left
-                    && isset($matrix[$i - 2][$j - 2]) && $matrix[$i - 2][$j - 2] === 'A'
-                    && isset($matrix[$i - 3][$j - 3]) && $matrix[$i - 3][$j - 3] === 'S'
+                if (
+                    isset($matrix[$i - 1][$j - 1], $matrix[$i - 2][$j - 2], $matrix[$i - 3][$j - 3])
+                    && $matrix[$i - 1][$j - 1] === 'M' // up-left
+                    && $matrix[$i - 2][$j - 2] === 'A'
+                    && $matrix[$i - 3][$j - 3] === 'S'
                 ) {
                     $solution++;
                 }
 
-                if (isset($matrix[$i - 1][$j + 1]) && $matrix[$i - 1][$j + 1] === 'M' // up-right
-                    && isset($matrix[$i - 2][$j + 2]) && $matrix[$i - 2][$j + 2] === 'A'
-                    && isset($matrix[$i - 3][$j + 3]) && $matrix[$i - 3][$j + 3] === 'S'
+                if (
+                    isset($matrix[$i - 1][$j + 1], $matrix[$i - 2][$j + 2], $matrix[$i - 3][$j + 3])
+                    && $matrix[$i - 1][$j + 1] === 'M' // up-right
+                    && $matrix[$i - 2][$j + 2] === 'A'
+                    && $matrix[$i - 3][$j + 3] === 'S'
                 ) {
                     $solution++;
                 }
 
-                if (isset($matrix[$i + 1][$j - 1]) && $matrix[$i + 1][$j - 1] === 'M' // down-left
-                    && isset($matrix[$i + 2][$j - 2]) && $matrix[$i + 2][$j - 2] === 'A'
-                    && isset($matrix[$i + 3][$j - 3]) && $matrix[$i + 3][$j - 3] === 'S'
+                if (
+                    isset($matrix[$i + 1][$j - 1], $matrix[$i + 2][$j - 2], $matrix[$i + 3][$j - 3])
+                    && $matrix[$i + 1][$j - 1] === 'M' // down-left
+                    && $matrix[$i + 2][$j - 2] === 'A'
+                    && $matrix[$i + 3][$j - 3] === 'S'
                 ) {
                     $solution++;
                 }
 
-                if (isset($matrix[$i + 1][$j + 1]) && $matrix[$i + 1][$j + 1] === 'M' // down-right
-                    && isset($matrix[$i + 2][$j + 2]) && $matrix[$i + 2][$j + 2] === 'A'
-                    && isset($matrix[$i + 3][$j + 3]) && $matrix[$i + 3][$j + 3] === 'S'
+                if (
+                    isset($matrix[$i + 1][$j + 1], $matrix[$i + 2][$j + 2], $matrix[$i + 3][$j + 3])
+                    && $matrix[$i + 1][$j + 1] === 'M' // down-right
+                    && $matrix[$i + 2][$j + 2] === 'A'
+                    && $matrix[$i + 3][$j + 3] === 'S'
                 ) {
                     $solution++;
                 }
@@ -106,26 +107,12 @@ final readonly class Day4 implements AdventOfCodeProblem
     public function secondPart(): int
     {
         $solution = 0;
-        $lineGenerator = DataReader::readLine(4, $this->withExampleData);
-        $matrix = [];
-        $aPositions = [];
-        foreach ($lineGenerator as $ix => $line) {
-            $row = str_split($line);
-            $matrix[$ix] = $row;
-            foreach ($row as $ic => $col) {
-                if ($col === 'A') {
-                    $aPositions[$ix][$ic] = true;
-                } else {
-                    $aPositions[$ix][$ic] = false;
-                }
-            }
-        }
+        [$matrix, $aPositions] = $this->getMatrixAndMainLetterPositions(searchForLetter: 'A');
 
         $rowCount = count($matrix);
         $colCount = count($matrix[0]);
         for ($i = 0; $i < $rowCount; $i++) {
             for ($j = 0; $j < $colCount; $j++) {
-                // optimize a bit only going through known A positions
                 if ($aPositions[$i][$j] === false) {
                     continue;
                 }
@@ -133,49 +120,66 @@ final readonly class Day4 implements AdventOfCodeProblem
                 // M . S
                 // . A .
                 // M . S
-                if (isset($matrix[$i - 1][$j - 1]) && $matrix[$i - 1][$j - 1] === 'M' // up-left
-                    && isset($matrix[$i - 1][$j + 1]) && $matrix[$i - 1][$j + 1] === 'S' // up-right
-                    && isset($matrix[$i + 1][$j - 1]) && $matrix[$i + 1][$j - 1] === 'M' // down-left
-                    && isset($matrix[$i + 1][$j + 1]) && $matrix[$i + 1][$j + 1] === 'S' // down-right
-                ) {
+                if ($this->hasPattern($matrix, $i, $j, 'M', 'S', 'M', 'S')) {
                     $solution++;
+                    continue;
                 }
 
                 // S . M
                 // . A .
                 // S . M
-                if (isset($matrix[$i - 1][$j - 1]) && $matrix[$i - 1][$j - 1] === 'S' // up-left
-                    && isset($matrix[$i - 1][$j + 1]) && $matrix[$i - 1][$j + 1] === 'M' // up-right
-                    && isset($matrix[$i + 1][$j - 1]) && $matrix[$i + 1][$j - 1] === 'S' // down-left
-                    && isset($matrix[$i + 1][$j + 1]) && $matrix[$i + 1][$j + 1] === 'M' // down-right
-                ) {
+                if ($this->hasPattern($matrix, $i, $j, 'S', 'M', 'S', 'M')) {
                     $solution++;
+                    continue;
                 }
 
                 // S . S
                 // . A .
                 // M . M
-                if (isset($matrix[$i - 1][$j - 1]) && $matrix[$i - 1][$j - 1] === 'S' // up-left
-                    && isset($matrix[$i - 1][$j + 1]) && $matrix[$i - 1][$j + 1] === 'S' // up-right
-                    && isset($matrix[$i + 1][$j - 1]) && $matrix[$i + 1][$j - 1] === 'M' // down-left
-                    && isset($matrix[$i + 1][$j + 1]) && $matrix[$i + 1][$j + 1] === 'M' // down-right
-                ) {
+                if ($this->hasPattern($matrix, $i, $j, 'S', 'S', 'M', 'M')) {
                     $solution++;
+                    continue;
                 }
 
                 // M . M
                 // . A .
                 // S . S
-                if (isset($matrix[$i - 1][$j - 1]) && $matrix[$i - 1][$j - 1] === 'M' // up-left
-                    && isset($matrix[$i - 1][$j + 1]) && $matrix[$i - 1][$j + 1] === 'M' // up-right
-                    && isset($matrix[$i + 1][$j - 1]) && $matrix[$i + 1][$j - 1] === 'S' // down-left
-                    && isset($matrix[$i + 1][$j + 1]) && $matrix[$i + 1][$j + 1] === 'S' // down-right
-                ) {
+                if ($this->hasPattern($matrix, $i, $j, 'M', 'M', 'S', 'S')) {
                     $solution++;
                 }
             }
         }
 
         return $solution;
+    }
+
+    public function getMatrixAndMainLetterPositions(string $searchForLetter): array
+    {
+        $lineGenerator = DataReader::readLine(4, $this->withExampleData);
+        $matrix = [];
+        $mainLetterPositions = [];
+
+        foreach ($lineGenerator as $i => $line) {
+            $row = str_split($line);
+            $matrix[$i] = $row;
+            foreach ($row as $j => $col) {
+                if ($col === $searchForLetter) {
+                    $mainLetterPositions[$i][$j] = true;
+                } else {
+                    $mainLetterPositions[$i][$j] = false;
+                }
+            }
+        }
+
+        return [$matrix, $mainLetterPositions];
+    }
+
+    private function hasPattern(array $matrix, int $rowIndex, int $colIndex, string $char1, string $char2, string $char3, string $char4): bool
+    {
+        return isset($matrix[$rowIndex - 1][$colIndex - 1], $matrix[$rowIndex - 1][$colIndex + 1], $matrix[$rowIndex + 1][$colIndex - 1], $matrix[$rowIndex + 1][$colIndex + 1])
+            && $matrix[$rowIndex - 1][$colIndex - 1] === $char1 // up-left
+            && $matrix[$rowIndex - 1][$colIndex + 1] === $char2 // up-right
+            && $matrix[$rowIndex + 1][$colIndex - 1] === $char3 // down-left
+            && $matrix[$rowIndex + 1][$colIndex + 1] === $char4; // down-right
     }
 }
