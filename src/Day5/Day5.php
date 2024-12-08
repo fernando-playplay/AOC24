03193e326855c4cdd0075321391e0c678770524f
Fernando AYON <fernando.ayon@playplay.com>
2024-12-08 21:09:31 +0100

    ✨ Solve day 5 parts 1 & 2

--- src/Day5/Day5.php
+++ src/Day5/Day5.php
@@ -17,8 +17,28 @@ final readonly class Day5 implements AdventOfCodeProblem
     {
         $solution = 0;
         $lineGenerator = DataReader::readLine(5, $this->withExampleData);
+        $ordering = [];
+        $updates = [];
+        $doPages = false;
         foreach ($lineGenerator as $line) {
-            // noop
+            if ($line === '') {
+                $doPages = true;
+                continue;
+            }
+            if ($doPages) {
+                $updates[] = array_map(static fn(string $val): int => (int) $val, explode(',', $line));
+            } else {
+                $order = explode('|', $line);
+                $ordering[(int) $order[0]][] = (int) $order[1];
+            }
+        }
+
+        foreach ($updates as $pages) {
+            $sortedPages = $pages;
+            usort($sortedPages, static fn(int $pageA, int $pageB): bool => in_array($pageA, $ordering[$pageB] ?? [], true));
+            if ($sortedPages === $pages) {
+                $solution += $pages[round((count($pages) - 1) / 2)];
+            }
         }
 
         return $solution;
@@ -26,6 +46,32 @@ final readonly class Day5 implements AdventOfCodeProblem
 
     public function secondPart(): int
     {
-        return 0;
+        $solution = 0;
+        $lineGenerator = DataReader::readLine(5, $this->withExampleData);
+        $ordering = [];
+        $updates = [];
+        $doPages = false;
+        foreach ($lineGenerator as $line) {
+            if ($line === '') {
+                $doPages = true;
+                continue;
+            }
+            if ($doPages) {
+                $updates[] = array_map(static fn(string $val): int => (int) $val, explode(',', $line));
+            } else {
+                $order = explode('|', $line);
+                $ordering[(int) $order[0]][] = (int) $order[1];
+            }
+        }
+
+        foreach ($updates as $pages) {
+            $sortedPages = $pages;
+            usort($sortedPages, static fn(int $pageA, int $pageB): bool => in_array($pageA, $ordering[$pageB] ?? [], true));
+            if ($sortedPages !== $pages) {
+                $solution += $sortedPages[round((count($sortedPages) - 1) / 2)];
+            }
+        }
+
+        return $solution;
     }
 }
--- tests/Day5/Day5Test.php
+++ tests/Day5/Day5Test.php
@@ -12,24 +12,24 @@ final class Day5Test extends TestCase
     public function testItSolvesTheExamplePart1(): void
     {
         // Act & Assert
-        $this->assertSame(0, new Day5(withExampleData: true)->firstPart());
+        $this->assertSame(143, new Day5(withExampleData: true)->firstPart());
     }
 
     public function testItSolvesPart1(): void
     {
         // Act & Assert
-        $this->assertSame(0, new Day5()->firstPart());
+        $this->assertSame(6498, new Day5()->firstPart());
     }
 
     public function testItSolvesTheExamplePart2(): void
     {
         // Act & Assert
-        $this->assertSame(0, new Day5(withExampleData: true)->secondPart());
+        $this->assertSame(123, new Day5(withExampleData: true)->secondPart());
     }
 
     public function testItSolvesPart2(): void
     {
         // Act & Assert
-        $this->assertSame(0, new Day5()->secondPart());
+        $this->assertSame(5017, new Day5()->secondPart());
     }
 }
